c848e60e6345ae8ed4dbd930e86bee48f3dc9d4a
Adrian <adrian@piso13.com>
2019-07-19 16:41:32 -0500

    Added project to repository
    Added project to repository

--- Flash Chat/CustomMessageCell.swift
+++ Flash Chat/CustomMessageCell.swift
@@ -2,8 +2,8 @@
 //  CustomMessageCell.swift
 //  Flash Chat
 //
-//  Created by Angela Yu on 30/08/2015.
-//  Copyright (c) 2015 London App Brewery. All rights reserved.
+//  Created by Adrian on 30/08/2015.
+//  Copyright (c) 2015 AdrianCruz. All rights reserved.
 //
 
 import UIKit
@@ -18,11 +18,11 @@ class CustomMessageCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code goes here
-        
+
         
         
     }
 
 
 }
+
